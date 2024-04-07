59f67827eb8add70cae721a24b8e017303e9eba1
kemberl1 <kemberligame@gmail.com>
2024-04-07 20:24:48 +0300

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+let swiperInstance = null; // Хранилище экземпляра Swiper
+
+function checkScreenWidth() {
+  if (window.innerWidth > 760) {
+    if (swiperInstance) {
+      console.log('swiper полностью удален');
+      swiperInstance.destroy(true, true);
+      swiperInstance = null;
+    }
+  } else {
+    if (!swiperInstance) {
+      swiperInstance = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: false,
+        slidesPerView: "auto",
+        spaceBetween: 16,
+        slideToClickedSlides: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        allowTouchMove: true,
+      });
+    }
+  }
+}
+
+window.addEventListener('load', checkScreenWidth);
+window.addEventListener('resize', checkScreenWidth);
+
+
+// show-more
+
+  let brandsButton = document.querySelector('.brands__button');
+  let brandsCardsList = document.querySelector('.brands__cards-list');
+
+  brandsButton.addEventListener('click', function () {
+    brandsButton.classList.toggle('brands__button--close');
+    brandsCardsList.classList.toggle('.brand__card-list--full');  
+  });
+  
+
+
+
+
+
