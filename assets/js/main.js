b350c34eebd74c445dd35539b45afdecebd5d74c
kemberl1 <kemberligame@gmail.com>
2024-04-08 21:11:50 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,49 @@
 document.addEventListener("DOMContentLoaded", function () {
-console.log('DOM загружен');
-let swiperInstance = null; // Хранилище экземпляра Swiper
+  let swiperInstance = null; //экземпляр свайпера
 
-function checkScreenWidth() {
-  if (window.innerWidth > 760) {
-    if (swiperInstance) {
-      console.log('swiper полностью удален');
-      swiperInstance.destroy(true, true);
-      swiperInstance = null;
-    }
-  } else {
-    if (!swiperInstance) {
-      swiperInstance = new Swiper('.swiper', {
-        direction: 'horizontal',
-        loop: false,
-        slidesPerView: "auto",
-        spaceBetween: 16,
-        slideToClickedSlides: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        allowTouchMove: true,
-      });
+  function checkScreenWidth() {
+    if (window.innerWidth > 760) {
+      if (swiperInstance) {
+        console.log("swiper полностью удален");
+        swiperInstance.destroy(true, true);
+        swiperInstance = null;
+      }
+    } else {
+      if (!swiperInstance) {
+        swiperInstance = new Swiper(".swiper", {
+          direction: "horizontal",
+          loop: false,
+          slidesPerView: "auto",
+          spaceBetween: 16,
+          slideToClickedSlides: true,
+          pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+          },
+          autoplay: {
+            delay: 2500,
+            disableOnInteraction: true,
+          },
+          allowTouchMove: true,
+        });
+      }
     }
   }
-}
+  window.addEventListener("load", checkScreenWidth);
+  window.addEventListener("resize", checkScreenWidth);
 
-window.addEventListener('load', checkScreenWidth);
-window.addEventListener('resize', checkScreenWidth);
+  /*showMoreButton*/
 
-// show-more
+  let showMoreButton = document.querySelector(".button__show-more");
+  let showFull = document.querySelector(".content-container");
 
-  let brandsButton = document.querySelector('.brands__button');
-  let brandsCardsList = document.querySelector('.brands__cards-list');
-
-  brandsButton.addEventListener('click', function () {
-    brandsButton.classList.toggle('brands__button--open');
-    brandsCardsList.classList.toggle('brand__card-list--full'); 
-    if (brandsButton.classList.contains('brands__button--open')) {
-      brandsButton.textContent = 'Скрыть';
+  showMoreButton.addEventListener("click", function () {
+    showMoreButton.classList.toggle("button__show-more--open");
+    showFull.classList.toggle("content-container-show--full");
+    if (showMoreButton.classList.contains("button__show-more--open")) {
+      showMoreButton.textContent = "Скрыть";
     } else {
-      brandsButton.textContent = 'Показать всё';
+      showMoreButton.textContent = "Показать всё";
     }
   });
-  
 });
